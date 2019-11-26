8919de5a5c1838327d6ba3effc3f18d5f2ddcc65
nalendraharva5 <48624976+nalendraharva5@users.noreply.github.com>
2019-11-26 15:40:12 +0700

    Add files via upload

---
+++ Hitung.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Hitung
+    {
+        public int Penjumlahan(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Pengurangan(int x, int y)
+        {
+            return x - y;
+        }
+        public int Perkalian(int x, int y)
+        {
+            return x * y;
+        }
+        public int Pembagian(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
